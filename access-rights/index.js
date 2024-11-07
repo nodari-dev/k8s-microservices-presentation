faeb8282ada4e1fffea4bf738348d49babf8d50e
nodari-dev <nodari.pylypyshak@gmail.com>
2024-11-07 11:19:27 +0100

    prep

--- access-rights/index.js
+++ access-rights/index.js
@@ -1,18 +1,61 @@
 const express = require('express');
 const axios = require('axios');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
-const port = 5000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', async (req, res) => {
-    try {
-        res.send(`Well, alright, you're good to go'`);
-    } catch (error) {
-        res.status(500).send('Error calling Service A');
-    }
+const PORT = 3001;
+const SERVICE_NAME = "access-rights"
+
+const LOGGER = "http://localhost:3000/log"
+
+const time_now = () => {
+	const date = new Date();
+
+	const year = date.getFullYear();
+	const month = String(date.getMonth() + 1).padStart(2, '0');
+	const day = String(date.getDate()).padStart(2, '0');
+	const hours = String(date.getHours()).padStart(2, '0');
+	const minutes = String(date.getMinutes()).padStart(2, '0');
+	const seconds = String(date.getSeconds()).padStart(2, '0');
+	const milliseconds = String(date.getMilliseconds()).padStart(3, '0');
+
+	return `${year}/${month}/${day} ${hours}:${minutes}:${seconds}.${milliseconds}`;
+}
+
+const log = async (time, method, service_name, endpoint, request_id) => {
+	try{
+		await axios.post(LOGGER, {time, method, service_name, endpoint, request_id})
+	} catch(e){
+		console.log(e)
+	}
+}
+
+const gen_req_id = (req) =>{
+	return req.headers["X-REQUEST-ID"] ?? uuidv4();
+}
+
+app.get('/public', async (req, res) => {
+	try {
+		await log(time_now, req.method, SERVICE_NAME, "/public", gen_req_id());
+		res.status(200);
+	} catch (error) {
+		res.status(500).send('something went wrong', e);
+	}
 });
 
-app.listen(port, () => {
-    console.log(`Service B running on port ${port}`);
+app.get('/public', async (req, res) => {
+	try {
+		await log(time_now, req.method, SERVICE_NAME, "/public", gen_req_id());
+		res.status(200);
+	} catch (error) {
+		res.status(500).send('something went wrong', e);
+	}
+});
+
+app.listen(PORT, () => {
+	console.log(`Access rights running on http://localhost:${PORT}`);
 });
 
--- storage/index.js
+++ storage/index.js
@@ -1,19 +1,88 @@
 const express = require('express');
 const axios = require('axios');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
-const port = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', async (req, res) => {
-    try {
-        const response = await axios.get('http://access-rights:5000/');
-        res.send(`Service B received: ${response.data}`);
-    } catch (error) {
-        res.status(500).send('Error calling Service A');
-    }
+const PORT = 3000;
+const SERVICE_NAME = "storage"
+
+const LOGGER = "http://localhost:3000/log"
+const ACCESS_RIGHTS = "http://localhost:3001"
+
+const time_now = () => {
+	const date = new Date();
+
+	const year = date.getFullYear();
+	const month = String(date.getMonth() + 1).padStart(2, '0');
+	const day = String(date.getDate()).padStart(2, '0');
+	const hours = String(date.getHours()).padStart(2, '0');
+	const minutes = String(date.getMinutes()).padStart(2, '0');
+	const seconds = String(date.getSeconds()).padStart(2, '0');
+	const milliseconds = String(date.getMilliseconds()).padStart(3, '0');
+
+	return `${year}/${month}/${day} ${hours}:${minutes}:${seconds}.${milliseconds}`;
+}
+
+const log = async (time, method, service_name, endpoint, request_id) => {
+	try{
+		await axios.post(LOGGER, {time, method, service_name, endpoint, request_id})
+	} catch(e){
+		console.log(e)
+	}
+}
+
+const check_access = async (endpoint) => {
+	let access = false;
+	try {
+		const res = axios.get(ACCESS_RIGHTS + endpoint)
+		if (res.status == 200) {
+			access = true
+		}
+	} catch (e) {
+		console.log("something happened", e)
+	}
+
+	return access
+}
+
+const gen_req_id = (req) =>{
+	return req.headers["X-REQUEST-ID"] ?? uuidv4();
+}
+
+app.get('/public', async (req, res) => {
+	try {
+		await log(time_now, req.method, SERVICE_NAME, "/public", gen_req_id());
+		const access_granted = check_access("/public");
+		if(access_granted){
+			res.send('alright, you can see public data');
+		} else{
+			res.status(403).send('You will not see public data');
+		}
+	} catch (error) {
+		console.error('Error calling access rights:', error.message);
+		res.status(500).send('error calling access rights');
+	}
 });
 
-app.listen(port, () => {
-    console.log(`Service B running on port ${port}`);
+app.get('/private', async (req, res) => {
+	try {
+		await log(time_now, req.method, SERVICE_NAME, "/private", gen_req_id());
+		const access_granted = check_access("/private");
+		if(access_granted){
+			res.send('alright, you can see private data');
+		} else{
+			res.status(403).send('You will not private data');
+		}
+	} catch (error) {
+		console.error('Error calling access rights:', error.message);
+		res.status(500).send('error calling access rights');
+	}
+});
+
+app.listen(PORT, () => {
+	console.log(`Storage running on http://localhost:${PORT}`);
 });
 
